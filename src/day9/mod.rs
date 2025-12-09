09bb48fb3301babab657c421ee76539b5e96e56c
Vitali Stsepaniuk <vitali.stsepaniuk@seven.one>
2025-12-09 10:32:53 +0100

    optimization

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -1,5 +1,6 @@
-use geo::geometry::{LineString, Point, Polygon};
 use geo::contains::Contains;
+use geo::geometry::{LineString, Point, Polygon};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use itertools::Itertools;
 use rayon::prelude::*;
@@ -18,43 +19,55 @@ fn parse(input: &str) -> Vec<(usize, usize)> {
 
 pub fn part1(input: &str) -> usize {
     parse(input)
-        .iter()
+        .into_iter()
         .tuple_combinations()
-        .map(|(p1, p2)| {
-            ((p1.0 as isize - p2.0 as isize).abs() + 1)
-                * ((p1.1 as isize - p2.1 as isize).abs() + 1)
-        })
+        .map(area)
         .max()
         .unwrap_or(0) as usize
+}
+
+#[inline]
+fn area((p1, p2): ((usize, usize), (usize, usize))) -> usize {
+    (((p1.0 as isize - p2.0 as isize).abs() + 1) * ((p1.1 as isize - p2.1 as isize).abs() + 1))
+        as usize
 }
 
 pub fn part2(input: &str) -> usize {
     let parsed = parse(input);
 
-
-    let points = parsed.iter()
+    let points = parsed
+        .iter()
         .map(|(x, y)| Point::new(*x as f32, *y as f32).0)
         .collect_vec();
 
     let p = Polygon::new(LineString::new(points), vec![]);
 
+    let max_area = AtomicUsize::new(0);
     parsed
         .into_iter()
         .tuple_combinations()
         .par_bridge()
-        .filter(|(p1, p2)| {
+        .filter_map(|(p1, p2)| {
+            let area = area((p1, p2));
+
+            if area <= max_area.load(Ordering::Relaxed) {
+                return None;
+            }
+
             let rect = geo::geometry::Rect::new(
                 Point::new(p1.0 as f32, p1.1 as f32),
                 Point::new(p2.0 as f32, p2.1 as f32),
             );
-            p.contains(&rect)
-        })
-        .map(|(p1, p2)| {
-            ((p1.0 as isize - p2.0 as isize).abs() + 1)
-                * ((p1.1 as isize - p2.1 as isize).abs() + 1)
+
+            if p.contains(&rect) {
+                max_area.fetch_max(area, Ordering::Relaxed);
+                Some(area)
+            } else {
+                None
+            }
         })
         .max()
-        .unwrap_or(0) as usize
+        .unwrap_or(0)
 }
 
 #[cfg(test)]
